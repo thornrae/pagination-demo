78690ee360d32624756c2a61d2999ec45e2b3cbb
Taylor Thornton <thornrae@gmail.com>
2022-01-11 23:10:43 -0800

    pagination demo, bootstrap CDN

---
+++ src/Components/Pagination.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Pagination = ({ totalPosts, postsPerPage, paginate }) => {
+  const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    pageNumbers.push(i);
+  }
+
+  return (
+    <nav>
+      <ul className="pagination">
+        {pageNumbers.map((number) => (
+          <li className="page-item" key={number}>
+            <a onClick={() => paginate(number)} href="!#" className="page-link">
+              {number}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Pagination;
